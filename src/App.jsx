439e19a29b298ae84b5935f41c103d04eeb9f5fe
michaelmeidenbauer <70448346+michaelmeidenbauer@users.noreply.github.com>
2021-09-14 20:24:06 -0500

    pivotal update

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
+        <h1>
+          JASON AND MICHAEL FUCKING RUUUUUULE
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
